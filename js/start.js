fac81b8dcceaa5e06159c79a18acfece132ea36b
KaramullinTimur <karamullint.h@gmail.com>
2021-10-12 11:14:46 +0600

    test_b3

--- js/start.js
+++ js/start.js
@@ -3,4 +3,5 @@ const button_wholesale = document.querySelector('.start-button-wholesale');
 
 button_wholesale.addEventListener('click', () => {
     start.classList.remove('active');
+    console.log('a')
 })
