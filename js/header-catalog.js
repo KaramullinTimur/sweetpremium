3e428a905912e55596c50cd8e312aa77f24eb1e4
KaramullinTimur <karamullint.h@gmail.com>
2021-10-12 09:55:25 +0600

    second

--- js/header-catalog.js
+++ js/header-catalog.js
@@ -1,6 +1,7 @@
 const catalog = document.querySelector('.header-catalog');
 const catalogButton = document.querySelector('.header-catalog-button');
 const catalogList = document.querySelector('.header-catalog-list');
+const catalogItems = document.querySelectorAll('.header-catalog-item');
 
 catalogButton.addEventListener('click', () => {
     catalogList.classList.toggle('active');
@@ -9,3 +10,21 @@ catalogButton.addEventListener('click', () => {
 catalog.addEventListener('mouseleave', () => {
     catalogList.classList.remove('active');
 })
+
+catalogItems.forEach(item => {
+    let catalogDropdown = item.querySelector('.header-catalog-dropdown');
+    item.addEventListener('click', () => {
+        catalogDropdown.classList.toggle('active');
+        
+        catalogDropdown.classList.add('animation-in');
+        catalogDropdown.classList.remove('animation-out');
+
+        // item.addEventListener('animationend', () => {
+        //     catalogDropdown.classList.remove('animation-in');
+        //     item.addEventListener('click', () => {
+        //         catalogDropdown.classList.add('animation-out');
+        //     })
+        // });
+    })
+})
+
