35cce5b6a18992b10972bd470aaeaa6cf5f78a67
KaramullinTimur <karamullint.h@gmail.com>
2021-10-12 10:53:08 +0600

    test_a

--- js/header-catalog.js
+++ js/header-catalog.js
@@ -11,20 +11,20 @@ catalog.addEventListener('mouseleave', () => {
     catalogList.classList.remove('active');
 })
 
-// catalogItems.forEach(item => {
-//     let catalogDropdown = item.querySelector('.header-catalog-dropdown');
-//     item.addEventListener('click', () => {
-//         catalogDropdown.classList.toggle('active');
+catalogItems.forEach(item => {
+    let catalogDropdown = item.querySelector('.header-catalog-dropdown');
+    item.addEventListener('click', () => {
+        catalogDropdown.classList.toggle('active');
         
-//         catalogDropdown.classList.add('animation-in');
-//         catalogDropdown.classList.remove('animation-out');
+        // catalogDropdown.classList.add('animation-in');
+        // catalogDropdown.classList.remove('animation-out');
 
-//         // item.addEventListener('animationend', () => {
-//         //     catalogDropdown.classList.remove('animation-in');
-//         //     item.addEventListener('click', () => {
-//         //         catalogDropdown.classList.add('animation-out');
-//         //     })
-//         // });
-//     })
-// })
+        // item.addEventListener('animationend', () => {
+        //     catalogDropdown.classList.remove('animation-in');
+        //     item.addEventListener('click', () => {
+        //         catalogDropdown.classList.add('animation-out');
+        //     })
+        // });
+    })
+})
 
